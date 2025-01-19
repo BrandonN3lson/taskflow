ce7fbad7bfa48f39a90969eedec999737ad3a899
Brandon Nelson <brandonnelson2@gmail.com>
2025-01-19 22:20:20 +0000

    add navbar component and styles for App and NavBar, instal react-router nad linked navbarlinks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 
-import './App.css';
 import { NavBar } from './components/NavBar';
-
+import styles from './App.module.css'
+import { Container } from 'react-bootstrap';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <NavBar/>
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={()=><h1>Dashboard</h1> } />
+          <Route exact path="/tasks" render={()=><h1>Tasks</h1> } />
+          <Route exact path="/signin" render={()=><h1>Sign in</h1> } />
+          <Route exact path="/signup" render={()=><h1>Sign up</h1> } />
+          <Route exact path="/logout" render={()=><h1>Log out</h1> } />
+          <Route render={()=><p>Page not found!</p> } />
+        </Switch>
+        
+        
+
+      </Container>
       
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,33 +1,75 @@
 import React from "react";
-import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
+
+import styles from "../styles/NavBar.module.css";
 
 export const NavBar = () => {
     return (
-        <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">TaskFlow</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar
 
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="#home">Dashboard</Nav.Link>
-                    <Nav.Link href="#link">Important</Nav.Link>
-                    <NavDropdown title="Category" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">
-                            Action
-                        </NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">
-                            Another action
-                        </NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">
-                            Something
-                        </NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">
-                            Separated link
-                        </NavDropdown.Item>
-                    </NavDropdown>
+            expand="md"
+            fixed="top"
+            className={`px-3 ${styles.NavBar}`}
+        >
+            <Container fluid width="100%">
+                <Navbar.Toggle aria-controls="navbar" className="navbar-dark"/>
+                <div>
+                    <Nav className="text-center d-none d-md-flex">
+                        <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/">
+                            Dashboard
+                        </NavLink>
+                        <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/tasks">
+                            Tasks
+                        </NavLink>
+                        <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">
+                            Sign up
+                        </NavLink>
+                        <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin">
+                            Sign in
+                        </NavLink>
+                        <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/logout">
+                            Log out
+                        </NavLink>
+                    </Nav>
+                </div>
+
+                <div>
+                    <NavLink to="/">
+                        <Navbar.Brand
+                            className={`mx-auto ${styles.Title}`}
+                            href="#home"
+                        >
+                            TaskFlow
+                        </Navbar.Brand>
+                    </NavLink>
+                </div>
+
+                <div>
+                    <Navbar.Text>
+                    <p className={styles.User}>User Logged In</p>
+                    </Navbar.Text>
+                </div>
+            </Container>
+
+            <Navbar.Collapse id="navbar">
+                <Nav className=" py-3 text-left d-md-none">
+                    <NavLink exact className={styles.ToggleNavLinks} activeClassName={styles.Active} to="/">
+                        Dashboard
+                    </NavLink>
+                    <NavLink className={styles.ToggleNavLinks} activeClassName={styles.Active} to="/tasks">
+                        Tasks
+                    </NavLink>
+                    <NavLink className={styles.ToggleNavLinks} activeClassName={styles.Active} to="/signup">
+                        Sign up
+                    </NavLink>
+                    <NavLink className={styles.ToggleNavLinks} activeClassName={styles.Active} to="/signin">
+                        Sign in
+                    </NavLink>
+                    <NavLink className={styles.ToggleNavLinks} activeClassName={styles.Active} to="/logout">
+                        Log out
+                    </NavLink>
                 </Nav>
-                <div>show user logged in</div>
             </Navbar.Collapse>
         </Navbar>
     );
